5b11582ab798e913f218b498b998b14e1e500ba9
Chris <cmlentz76@gmail.com>
2021-07-11 08:58:21 -0400

    completed assignment

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  # Class body
+
+  # Instance Method Definition
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+end
+
+Chris = Person.new
+Chris.talk
+Chris.walk
